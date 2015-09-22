ccba7f749c1e7fc9637fb6269e32a3b0126fe73b
Peter Juras <p.juras@outlook.com>
2015-09-22 18:06:30 +0200

    Add debug-strippers and uglifiers with a production setting

--- templates/javascript/gulpfile.js
+++ templates/javascript/gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var yargs = require('yargs').argv;
+var gulpIf = require('gulp-if');
 var merge = require('merge-stream');
+var uglify = require('gulp-uglify');
+var stripDebug = require('gulp-strip-debug');
+var gulpFilter = require('gulp-filter');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
 var install = require('gulp-install');
 
 gulp.task('build', ['copy:client', 'copy:server'], function() {
@@ -10,18 +17,38 @@ gulp.task('build', ['copy:client', 'copy:server'], function() {
 });
 
 gulp.task('copy:client', function() {
+  var jsFilter = gulpFilter('**/*.js', { restore: true });
+  var htmlFilter = gulpFilter('**/*.html', { restore: true });
+  var cssFilter = gulpFilter('**/*.css', { restore: true });
+
   return gulp.src('src/**')
+    .pipe(jsFilter)
+    .pipe(gulpIf(yargs.production, stripDebug()))
+    .pipe(gulpIf(yargs.production, uglify()))
+    .pipe(jsFilter.restore)
+    .pipe(htmlFilter)
+    .pipe(gulpIf(yargs.production, minifyHtml()))
+    .pipe(htmlFilter.restore)
+    .pipe(cssFilter)
+    .pipe(gulpIf(yargs.production, minifyCss()))
+    .pipe(cssFilter.restore)
     .pipe(gulp.dest('build/public/'));
 });
 
 gulp.task('copy:server', function() {
+  var jsFilter = gulpFilter('**/*.js', { restore: true });
+
   return gulp.src([
     './**',
     '!src', '!src/**',
     '!gulpfile.js',
-    '!node_modules', '!node_modules/**'
+    '!node_modules', '!node_modules/**',
+    '!build', '!build/**'
     ], {
     root: './'
     })
+    .pipe(jsFilter)
+    .pipe(gulpIf(yargs.production, stripDebug()))
+    .pipe(jsFilter.restore)
     .pipe(gulp.dest('build/'));
 });
--- templates/typescript/gulpfile.js
+++ templates/typescript/gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
+var yargs = require('yargs').argv;
+var gulpIf = require('gulp-if');
 var merge = require('merge-stream');
 var del = require('del');
 var install = require('gulp-install');
+var uglify = require('gulp-uglify');
+var stripDebug = require('gulp-strip-debug');
+var gulpFilter = require('gulp-filter');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
 var ts = require('gulp-typescript');
 
 var tsProject = ts.createProject('tsconfig.json');
@@ -15,35 +22,36 @@ gulp.task('build', ['move:src'], function() {
     }));
 });
 
-gulp.task('copy:client', function() {
-  var js = gulp.src('src/**.ts',
-    {
-      root: './'
-    })
-    .pipe(ts(tsProject))
-    .js
-    .pipe(gulp.dest('build/public/'));
-
-  var html = gulp.src(['src/**', '!src/**/**.ts'], {root: './'})
-    .pipe(gulp.dest('build/public/'));
-
-  return merge(js, html);
-});
-
 gulp.task('move:src', ['copy'], function() {
+  var jsFilter = gulpFilter('**/*.js', { restore: true });
+  var htmlFilter = gulpFilter('**/*.html', { restore: true });
+  var cssFilter = gulpFilter('**/*.css', { restore: true });
+
   return gulp.src('./build/src/**/**')
+    .pipe(jsFilter)
+    .pipe(gulpIf(yargs.production, stripDebug()))
+    .pipe(gulpIf(yargs.production, uglify()))
+    .pipe(jsFilter.restore)
+    .pipe(htmlFilter)
+    .pipe(gulpIf(yargs.production, minifyHtml()))
+    .pipe(htmlFilter.restore)
+    .pipe(cssFilter)
+    .pipe(gulpIf(yargs.production, minifyCss()))
+    .pipe(cssFilter.restore)
     .pipe(gulp.dest('./build/public'));
 });
 
 gulp.task('copy', function() {
   var js = gulp.src([
     '**/**.ts',
-    '!node_modules', '!node_modules/**'
+    '!node_modules', '!node_modules/**',
+    '!build', '!build/**'
     ], {
       root: './'
     })
     .pipe(ts(tsProject))
     .js
+    .pipe(gulpIf(yargs.production, stripDebug()))
     .pipe(gulp.dest('build/'));
 
   var misc = gulp.src([
@@ -53,7 +61,8 @@ gulp.task('copy', function() {
     '!node_modules', '!node_modules/**',
     '!build', '!build/**',
     '!typings', '!typings/**',
-    '!tsd.json', '!tsconfig.json'
+    '!tsd.json', '!tsconfig.json',
+    '!build', '!build/**'
     ], {
       root: './'
     })
