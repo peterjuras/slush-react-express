408fc126665d6ac5da5bfc69b08f28d3c589dcb6
Peter Juras <p.juras@outlook.com>
2015-09-28 23:51:18 +0200

    Make inquirer questions easier to answer

--- slushfile.js
+++ slushfile.js
@@ -1,44 +1,50 @@
 var gulp = require('gulp'),
-    install = require('gulp-install'),
-    conflict = require('gulp-conflict'),
-    template = require('gulp-template'),
-    es = require('event-stream'),
-    inquirer = require('inquirer'),
-    rename = require('gulp-rename'),
-    emptyDir = require('empty-dir'),
-    run = require('run-sequence'),
-    validateName = require('validate-npm-package-name'),
-    tsd = require('gulp-tsd');
+  install = require('gulp-install'),
+  conflict = require('gulp-conflict'),
+  template = require('gulp-template'),
+  es = require('event-stream'),
+  inquirer = require('inquirer'),
+  rename = require('gulp-rename'),
+  emptyDir = require('empty-dir'),
+  run = require('run-sequence'),
+  validateName = require('validate-npm-package-name'),
+  tsd = require('gulp-tsd');
 
 var destination = process.env.testDest || './';
 
 gulp.task('default', ['generate']);
 
-gulp.task('generate', function(callback) {
-  inquirer.prompt([ {
-      type: 'input',
-      name: 'type',
-      message: 'Would you like to use a minimal or a full (gulp builds, tests, production settings) template? (full/minimal)',
-      default: 'minimal'
-    }], function(answer) {
-      if (answer.type.indexOf('full') != -1) {
-        run('generate:full', callback);
-      } else {
-        run('generate:minimal', callback);
-      }
-    });
+gulp.task('generate', function (callback) {
+  console.log('Welcome to the react-express slush generator!');
+  inquirer.prompt([{
+    type: 'list',
+    name: 'type',
+    message: 'Would you like to use a minimal or a full (gulp builds, tests, production settings) template?',
+    choices: ['Minimal', 'Full'],
+    default: 0
+  }], function (answer) {
+    if (answer.type.indexOf('Full') != -1) {
+      run('generate:full', callback);
+    } else {
+      run('generate:minimal', callback);
+    }
+  });
 });
 
 gulp.task('generate:minimal', function (done) {
   var workingDirName = process.cwd().split('/').pop().split('\\').pop();
 
   emptyDir('./', function (err, dirEmpty) {
-    var questions = [{ 
-      type: 'input', name: 'appname', message: 'What will your app be called?', default: workingDirName
+    var questions = [{
+      type: 'input',
+      name: 'appname',
+      message: 'What will your app be called?',
+      default: formatPackageName(workingDirName.toLowerCase()),
+      validate: validateNameAnswer
     }];
     if (destination === './' && !dirEmpty) {
       questions.push({
-        type: 'input', name: 'createDir', message: 'The current folder is not empty, do you want to create a new folder? (yes/no)', default: 'yes'
+        type: 'confirm', name: 'createDir', message: 'The current folder is not empty, do you want to create a new folder?', default: true
       });
     }
     inquirer.prompt(questions, function (answers) {
@@ -48,13 +54,13 @@ gulp.task('generate:minimal', function (done) {
 
       answers = cleanName(answers);
 
-      if (answers.createDir && answers.createDir === 'yes') {
+      if (answers.createDir && answers.createDir) {
         destination = destination + answers.appname + '/';
       }
 
       gulp.src(__dirname + '/templates/minimal/**', { dot: true })  // Note use of __dirname to be relative to generator
         .pipe(template(answers))                 // Lodash template support
-        .pipe(rename(function(path) {
+        .pipe(rename(function (path) {
           if (path.basename === '.npmignore') {
             path.basename = '.gitignore';
           }
@@ -73,13 +79,33 @@ gulp.task('generate:full', function (done) {
 
   emptyDir('./', function (err, dirEmpty) {
     var questions = [
-      {type: 'input', name: 'appname', message: 'What will your app be called?', default: workingDirName},
-      {type: 'input', name: 'ts', message: 'typescript or javascript?', default: 'javascript'},
-      {type: 'input', name: 'sass', message: 'sass or css?', default: 'css'}
+      {
+        type: 'input',
+        name: 'appname',
+        message: 'What will your app be called?',
+        default: formatPackageName(workingDirName.toLowerCase()),
+        validate: validateNameAnswer
+      }, {
+        type: 'list',
+        name: 'ts',
+        message: 'Do you want to write in JavaScript or TypeScript?',
+        choices: [
+          'JavaScript', 'TypeScript'
+        ],
+        default: 0
+      }, {
+        type: 'list',
+        name: 'sass',
+        message: 'Do you want to style in CSS or SASS?',
+        choices: [
+          'CSS', 'SASS'
+        ],
+        default: 0
+      }
     ];
     if (destination === './' && !dirEmpty) {
       questions.push({
-        type: 'input', name: 'createDir', message: 'The current folder is not empty, do you want to create a new folder? (yes/no)', default: 'yes'
+        type: 'confirm', name: 'createDir', message: 'The current folder is not empty, do you want to create a new folder?', default: true
       });
     }
     inquirer.prompt(questions, function (answers) {
@@ -91,7 +117,7 @@ gulp.task('generate:full', function (done) {
       answers = addPackages(answers);
       answers = cleanName(answers);
 
-      if (answers.createDir && answers.createDir === 'yes') {
+      if (answers.createDir && answers.createDir) {
         destination = destination + answers.appname + '/';
       }
 
@@ -102,8 +128,8 @@ gulp.task('generate:full', function (done) {
       }
 
       gulp.src([__dirname + '/templates/full/' + scriptDir + '/**',
-          '!' + __dirname + '/templates/full/' + scriptDir + '/typings',
-          '!' + __dirname + '/templates/full/' + scriptDir + '/typings/**'])  // Note use of __dirname to be relative to generator
+        '!' + __dirname + '/templates/full/' + scriptDir + '/typings',
+        '!' + __dirname + '/templates/full/' + scriptDir + '/typings/**'])  // Note use of __dirname to be relative to generator
         .pipe(template(answers, { interpolate: /<%=([\s\S]+?)%>/g }))                 // Lodash template support
         .pipe(conflict(destination))                    // Confirms overwrites on file conflicts
         .pipe(gulp.dest(destination));                   // Without __dirname here = relative to cwd
@@ -114,7 +140,7 @@ gulp.task('generate:full', function (done) {
 
       answers.sassFilter = '';
       answers.sassPipe = '';
-      if (answers.sass.indexOf('sass') != -1) {
+      if (answers.sass.indexOf('SASS') != -1) {
         gulp.src(__dirname + '/templates/full/sass/**')
           .pipe(conflict(destination + 'src'))
           .pipe(gulp.dest(destination + 'src'));
@@ -128,11 +154,11 @@ gulp.task('generate:full', function (done) {
       }
 
       gulp.src([
-          __dirname + '/templates/full/root/**',
-          '!' + __dirname + '/templates/full/root/src/favicon.ico',
-        ], { dot: true })  // Note use of __dirname to be relative to generator
+        __dirname + '/templates/full/root/**',
+        '!' + __dirname + '/templates/full/root/src/favicon.ico',
+      ], { dot: true })  // Note use of __dirname to be relative to generator
         .pipe(template(answers))                 // Lodash template support
-        .pipe(rename(function(path) {
+        .pipe(rename(function (path) {
           if (path.basename === '.npmignore') {
             path.basename = '.gitignore';
           }
@@ -143,8 +169,8 @@ gulp.task('generate:full', function (done) {
         .on('end', function () {
           if (useTypescript(answers)) {
             tsd({
-                command: 'reinstall',
-                config: destination + 'tsd.json'
+              command: 'reinstall',
+              config: destination + 'tsd.json'
             }, done);
           } else {
             done();       // Finished!
@@ -174,21 +200,33 @@ function addPackages(answers) {
     devPackages.push(['gulp-sass', '^2.0.4']);
   }
 
-  packages.forEach(function(package, index) {
+  packages.forEach(function (package, index) {
     answers.packages += packageDelimiter + '"' + package[0] + '": "' + package[1] + '"';
   });
-  devPackages.forEach(function(package, index) {
+  devPackages.forEach(function (package, index) {
     answers.devPackages += packageDelimiter + '"' + package[0] + '": "' + package[1] + '"';
   });
   return answers;
 }
 
 function useTypescript(answers) {
-  return answers.ts.indexOf('typescript') != -1 ||
-    answers.ts.indexOf('ts') != -1;
+  return answers.ts.indexOf('TypeScript') != -1;
 }
 
 function cleanName(answers) {
-  answers.appname = answers.appname.split(' ').join('-');
+  answers.appname = formatPackageName(answers.appname);
   return answers;
+}
+
+function formatPackageName(name) {
+  return name.split(' ').join('-');
+}
+
+function validateNameAnswer(input) {
+  var validation = validateName(input);
+  if (validation.validForNewPackages) {
+    return true;
+  } else {
+    return 'Your name contains illegal characters. Please choose another name!';
+  }
 }
