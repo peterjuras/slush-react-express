81dca498d7fde09769854fbbb89f523c661c87c1
Peter Juras <p.juras@outlook.com>
2016-07-02 10:57:23 +0200

    Update tests to account for known gulp bug with graceful-fs

--- test/full-builds.ts
+++ test/full-builds.ts
@@ -10,154 +10,159 @@ const exec = childProcess.exec;
 import generate from '../slushfile';
 const buildPath = path.join(__dirname, 'full-builds');
 
-describe('slush-react-express', function () {
-  this.timeout(1000 * 60 * 30);
+function checkForWarnings(stderr: string) {
+    return stderr.toString().split('\n')
+        .every(line =>
+            line.indexOf('npm WARN') === 0 || // npm warnings
+            line.indexOf('fs: re-evaluating native module sources is not supported. If you are using the graceful-fs module, please update it to a more recent version.') !== -1 || // confirmed gulp bug
+            !/\S/.test(line) // whitespace
+        );
+}
 
-  before(done => {
-    process.chdir(__dirname);
-    del.sync('full-builds');
-    mkdirp(buildPath, done);
-  });
+describe('slush-react-express', function() {
+    this.timeout(1000 * 60 * 30);
 
-  describe('Minimal generator', () => {
-    beforeEach(() => {
-      process.chdir(buildPath);
-
-      mockPrompt({
-        'appname': 'Minimal js',
-        'type': 'Minimal',
-        'createDir': true
-      });
+    before(done => {
+        process.chdir(__dirname);
+        del.sync('full-builds');
+        mkdirp(buildPath, done);
     });
 
-    it('should scaffold without error', done => {
-      generate(done);
-    });
-  });
+    describe('Minimal generator', () => {
+        beforeEach(() => {
+            process.chdir(buildPath);
 
-  describe('Full generator', () => {
-    beforeEach(() => {
-      process.chdir(buildPath);
-    });
+            mockPrompt({
+                'appname': 'Minimal js',
+                'type': 'Minimal',
+                'createDir': true
+            });
+        });
 
-    describe('JavaScript template', () => {
-      beforeEach(() => {
-        process.chdir(buildPath);
-      });
-
-      it('css', done => {
-        mockPrompt({
-          'appname': 'FULL JS CSS',
-          'type': 'Full',
-          'ts': 'JavaScript',
-          'sass': 'CSS',
-          'createDir': true
+        it('should scaffold without error', done => {
+            generate(done);
         });
-        const templateDest = 'full-js-css';
-        generate(() => {
-          process.chdir(path.join(buildPath, templateDest));
-          exec('node_modules/.bin/gulp', (error, stdout, stderr) => {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-            if (error) {
-              console.log('exec error: ' + error);
-              done(error);
-            }
-            if (stderr) {
-              const onlyWarnings = stderr.toString().split('\n')
-                .every(line => line.indexOf('npm WARN') === 0 || !/\S/.test(line));
-              done(!onlyWarnings);
-            }
-          });
-        });
-      });
-
-      it('sass', done => {
-        mockPrompt({
-          'appname': 'FULL JS SASS',
-          'type': 'Full',
-          'ts': 'JavaScript',
-          'sass': 'SASS',
-          'createDir': true
-        });
-        const templateDest = 'full-js-sass';
-        generate(() => {
-          process.chdir(path.join(buildPath, templateDest));
-          exec('node_modules/.bin/gulp', (error, stdout, stderr) => {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-            if (error) {
-              console.log('exec error: ' + error);
-              done(error);
-            }
-            if (stderr) {
-              const onlyWarnings = stderr.toString().split('\n')
-                .every(line => line.indexOf('npm WARN') === 0 || !/\S/.test(line));
-              done(!onlyWarnings);
-            }
-          });
-        });
-      });
     });
 
-    describe('TypeScript template', () => {
-      beforeEach(() => {
-        process.chdir(buildPath);
-      });
+    describe('Full generator', () => {
+        beforeEach(() => {
+            process.chdir(buildPath);
+        });
 
-      it('css', done => {
-        mockPrompt({
-          'appname': 'FULL TS CSS',
-          'type': 'Full',
-          'ts': 'TypeScript',
-          'sass': 'CSS',
-          'createDir': true
-        });
-        const templateDest = 'full-ts-css';
-        generate(() => {
-          process.chdir(path.join(buildPath, templateDest));
-          exec('node_modules/.bin/gulp', (error, stdout, stderr) => {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-            if (error) {
-              console.log('exec error: ' + error);
-              done(error);
-            }
-            if (stderr) {
-              const onlyWarnings = stderr.toString().split('\n')
-                .every(line => line.indexOf('npm WARN') === 0 || !/\S/.test(line));
-              done(!onlyWarnings);
-            }
-          });
-        });
-      });
+        describe('JavaScript template', () => {
+            beforeEach(() => {
+                process.chdir(buildPath);
+            });
 
-      it('sass', done => {
-        mockPrompt({
-          'appname': 'FULL TS SASS',
-          'type': 'Full',
-          'ts': 'TypeScript',
-          'sass': 'SASS',
-          'createDir': true
+            it('css', done => {
+                mockPrompt({
+                    'appname': 'FULL JS CSS',
+                    'type': 'Full',
+                    'ts': 'JavaScript',
+                    'sass': 'CSS',
+                    'createDir': true
+                });
+                const templateDest = 'full-js-css';
+                generate(() => {
+                    process.chdir(path.join(buildPath, templateDest));
+                    exec('node_modules/.bin/gulp', (error, stdout, stderr) => {
+                        console.log('stdout: ' + stdout);
+                        console.log('stderr: ' + stderr);
+                        if (error) {
+                            console.log('exec error: ' + error);
+                            done(error);
+                        }
+                        if (stderr) {
+                            const onlyWarnings = checkForWarnings(stderr);
+                            done(!onlyWarnings);
+                        }
+                    });
+                });
+            });
+
+            it('sass', done => {
+                mockPrompt({
+                    'appname': 'FULL JS SASS',
+                    'type': 'Full',
+                    'ts': 'JavaScript',
+                    'sass': 'SASS',
+                    'createDir': true
+                });
+                const templateDest = 'full-js-sass';
+                generate(() => {
+                    process.chdir(path.join(buildPath, templateDest));
+                    exec('node_modules/.bin/gulp', (error, stdout, stderr) => {
+                        console.log('stdout: ' + stdout);
+                        console.log('stderr: ' + stderr);
+                        if (error) {
+                            console.log('exec error: ' + error);
+                            done(error);
+                        }
+                        if (stderr) {
+                            const onlyWarnings = checkForWarnings(stderr);
+                            done(!onlyWarnings);
+                        }
+                    });
+                });
+            });
         });
-        const templateDest = 'full-ts-sass';
-        generate(() => {
-          process.chdir(path.join(buildPath, templateDest));
-          exec('node_modules/.bin/gulp', (error, stdout, stderr) => {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-            if (error) {
-              console.log('exec error: ' + error);
-              done(error);
-            }
-            if (stderr) {
-              const onlyWarnings = stderr.toString().split('\n')
-                .every(line => line.indexOf('npm WARN') === 0 || !/\S/.test(line));
-              done(!onlyWarnings);
-            }
-          });
+
+        describe('TypeScript template', () => {
+            beforeEach(() => {
+                process.chdir(buildPath);
+            });
+
+            it('css', done => {
+                mockPrompt({
+                    'appname': 'FULL TS CSS',
+                    'type': 'Full',
+                    'ts': 'TypeScript',
+                    'sass': 'CSS',
+                    'createDir': true
+                });
+                const templateDest = 'full-ts-css';
+                generate(() => {
+                    process.chdir(path.join(buildPath, templateDest));
+                    exec('node_modules/.bin/gulp', (error, stdout, stderr) => {
+                        console.log('stdout: ' + stdout);
+                        console.log('stderr: ' + stderr);
+                        if (error) {
+                            console.log('exec error: ' + error);
+                            done(error);
+                        }
+                        if (stderr) {
+                            const onlyWarnings = checkForWarnings(stderr);
+                            done(!onlyWarnings);
+                        }
+                    });
+                });
+            });
+
+            it('sass', done => {
+                mockPrompt({
+                    'appname': 'FULL TS SASS',
+                    'type': 'Full',
+                    'ts': 'TypeScript',
+                    'sass': 'SASS',
+                    'createDir': true
+                });
+                const templateDest = 'full-ts-sass';
+                generate(() => {
+                    process.chdir(path.join(buildPath, templateDest));
+                    exec('node_modules/.bin/gulp', (error, stdout, stderr) => {
+                        console.log('stdout: ' + stdout);
+                        console.log('stderr: ' + stderr);
+                        if (error) {
+                            console.log('exec error: ' + error);
+                            done(error);
+                        }
+                        if (stderr) {
+                            const onlyWarnings = checkForWarnings(stderr);
+                            done(!onlyWarnings);
+                        }
+                    });
+                });
+            });
         });
-      });
     });
-  });
 });
